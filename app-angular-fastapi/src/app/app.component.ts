258e755cffacc36afe786a4fa779340fe5a332b4
Benoit VIGNAUD <benoit.vignaud2@gmail.com>
2025-05-15 15:37:04 +0200

    ajout srevice

--- app-angular-fastapi/src/app/app.component.ts
+++ app-angular-fastapi/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TraitementImageService } from './services/traitement-image.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  traitementImagesService: TraitementImageService;
   title = 'Angular FastAPI';
+  url!: string | ArrayBuffer
+  formDataImage: FormData = new FormData();
+
+  constructor(traitementImagesService: TraitementImageService) {
+    this.traitementImagesService = traitementImagesService;
+  }
+
+  onFileChanged(event) {
+    let reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.url = reader.result;
+        this.formDataImage.append('file', file);
+
+      };
+    }
+  }
+
 }
---
+++ app-angular-fastapi/src/app/services/traitement-image.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TraitementImageService {
+
+  constructor() { }
+
+  public increaseImageResolution(imageFormData: FormData) {
+   
+  }
+}
